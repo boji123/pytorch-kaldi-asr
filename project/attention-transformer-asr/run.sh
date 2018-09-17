921c9b483f418bf299c88414034d4a3d6ace6fb7
boji <boji@aliyun.com>
2018-09-17 14:59:14 +0800

    加入文件检测

--- project/attention-transformer-asr/run.sh
+++ project/attention-transformer-asr/run.sh
@@ -2,6 +2,7 @@
 #----------------------------------------
 #edited by baiji
 #an example for pytorch training
+#----------------------------------------
 #----------        path        ----------
 MY_PYTORCH_KALDI_DIR=$HOME/work/pytorch-kaldi-asr
 export PATH=$MY_PYTORCH_KALDI_DIR:$MY_PYTORCH_KALDI_DIR/kaldi:$MY_PYTORCH_KALDI_DIR/pytorch:$PATH
@@ -17,8 +18,18 @@ export cuda_cmd="queue.pl -q GPU_QUEUE@@amax2017 -l gpu=1"
 export cuda_cmd="queue.pl -q GPU_QUEUE@compute-0-5.local -l gpu=1"
 #----------------------------------------
 set -e # exit on error
+#----------------------------------------
+
+
+#notice: step of data preparation here is done by kaldi, so I just copy the data files to data/
+if [ -d data ]&&[ -f data/feats.scp ]&&[ -f data/text ]; then
+    echo '[INFO] trainning data founded, continue trainning process.'
+else
+    echo '[ERROR] requir file not founded, please check your directory'
+    exit 1
+fi
+
 
 #python3 local/prepare_vocab.py -read_instances_file data/text -save_vocab_file exp/vocab.torch
 
 python3 test.py
-
