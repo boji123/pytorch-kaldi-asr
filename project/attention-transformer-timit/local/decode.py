68eb12a77e00c6896f77d2bb599c1df3c15e22a5
boji <boji@aliyun.com>
2019-02-22 10:27:11 +0800

    重写了decode代码，未完成，将在未来加入lattice生成功能

--- project/attention-transformer-timit/local/decode.py
+++ project/attention-transformer-timit/local/decode.py
@@ -12,93 +12,46 @@ import torch.nn as nn
 
 from torch.autograd import Variable
 from transformer.Models import Transformer
+from transformer.Models import fold_seq_and_mask
 from transformer.Beam import Beam
 
-def initialize_batch_loader(read_feats_scp_file, read_text_file, read_vocab_file, batch_size):
-    utterances = {}
-    with open(read_feats_scp_file, encoding='utf-8') as file:
-        for line in file:
-            (key,rxfile) = line.split()
-            utterances[key] = rxfile
-            #the utterance can be read by 
-            #mat = kaldi_io.read_mat(rxfile)
-
-    print('[INFO] get {} utterances from {}.'.format(len(utterances), read_feats_scp_file))
-
-    label_text = {}
-    with open(read_text_file, encoding='utf-8') as file:
-        for line in file:
-            data = line.split()
-            key = data[0]
-            text = data[1:]
-            label_text[key] = text
-    print('[INFO] get {} labels from {}.'.format(len(label_text), read_text_file))
-
-    #begin & end of sequence
-    label_text = instances_handler.add_control_words(label_text)
-    label = instances_handler.apply_vocab(label_text, read_vocab_file, 'word2idx')
-
-    #connect the label and utterances
-    trainning_triples = []
-    for key in utterances:
-        if key in label:
-            triples = (key, utterances[key], label[key])
-            trainning_triples.append(triples)
-    print('[INFO] match {} utterance-label pairs.'.format(len(trainning_triples)))
-
-    #in batch loader, trainning feature will be loaded while itering
-    #it increase io but decrease memory use
-    batch_loader = BatchLoader(trainning_triples, batch_size, print_info = False)
-    return batch_loader
-
+from train import initialize_batch_loader
 
 def translate_batch(model, batch, opt, model_options):
     model.eval()
     # prepare data
-    #key = [triples[0] for triples in batch]
-    src = [triples[1] for triples in batch]
-    tgt = [triples[2] for triples in batch]
-    src_seq, src_pad_mask = instances_handler.pad_to_longest(src)
-    tgt_seq, tgt_pad_mask = instances_handler.pad_to_longest(tgt)
+    src_seq = batch[1]
+    src_pad_mask = batch[2]
+    tgt_seq = batch[3]
 
-    src_seq = Variable(torch.FloatTensor(src_seq)) #batch * max length in batch * padded feature dim
-    src_pad_mask = Variable(torch.LongTensor(src_pad_mask)) #batch * maxlength in batch * bool mask dim
-    tgt_seq = Variable(torch.LongTensor(tgt_seq)) #batch * max length in batch * padded index dim
-    tgt_pad_mask = Variable(torch.LongTensor(tgt_pad_mask)) #batch * maxlength in batch * bool mask dim
+    src_seq = torch.FloatTensor(src_seq) #batch * max length in batch * padded feature dim
+    src_pad_mask = torch.ByteTensor(src_pad_mask) #batch * maxlength in batch * bool mask dim
+    # warning: embedding require long tensor, maybe it's a waste of menory, waiting to be solved
+    tgt_seq = torch.LongTensor(tgt_seq) #batch * max length in batch * padded index dim
 
     if opt.use_gpu:
         src_seq = src_seq.cuda()
         src_pad_mask = src_pad_mask.cuda()
         tgt_seq = tgt_seq.cuda()
-        tgt_pad_mask = tgt_pad_mask.cuda()
 
     goal = tgt_seq[:, 1:]
     tgt_seq = tgt_seq[:, :-1]
-    tgt_pad_mask = tgt_pad_mask[:, :-1]
 
     beam_size = opt.beam_size
     batch_size = src_seq.size(0)
     #---------------------------------------------------------------------------------------
     #- Enocde
+    src_seq, src_pad_mask = fold_seq_and_mask(src_seq, src_pad_mask, model.src_fold)
     enc_output, *_ = model.encoder(src_seq, src_pad_mask)
 
     #--- Repeat data for beam
-    src_seq = Variable(
-        src_seq.data.repeat(1, beam_size, 1).view(
-            src_seq.size(0) * beam_size, src_seq.size(1), src_seq.size(2)))
-
-    src_pad_mask = Variable(
-        src_pad_mask.data.repeat(1, beam_size).view(
-            src_pad_mask.size(0) * beam_size, src_pad_mask.size(1)))
-
-    enc_output = Variable(
-        enc_output.data.repeat(1, beam_size, 1).view(
-            enc_output.size(0) * beam_size, enc_output.size(1), enc_output.size(2)))
+    src_seq = src_seq.data.repeat(1, beam_size, 1).view(src_seq.size(0) * beam_size, src_seq.size(1), src_seq.size(2))
+    src_pad_mask = src_pad_mask.data.repeat(1, beam_size).view(src_pad_mask.size(0) * beam_size, src_pad_mask.size(1))
+    enc_output = enc_output.data.repeat(1, beam_size, 1).view(enc_output.size(0) * beam_size, enc_output.size(1), enc_output.size(2))
 
     #--- Prepare beams
     beams = [Beam(beam_size, opt.use_gpu) for _ in range(batch_size)]
-    beam_inst_idx_map = {
-        beam_idx: inst_idx for inst_idx, beam_idx in enumerate(range(batch_size))}
+    beam_inst_idx_map = {beam_idx: inst_idx for inst_idx, beam_idx in enumerate(range(batch_size))}
     n_remaining_sents = batch_size
 
     #- Decode
@@ -111,8 +64,8 @@ def translate_batch(model, batch, opt, model_options):
         dec_partial_seq, dec_partial_seq_mask = instances_handler.pad_to_longest(dec_partial_seq)
 
         # size: (batch * beam) x seq
-        dec_partial_seq = Variable(torch.LongTensor(dec_partial_seq))
-        dec_partial_seq_mask = Variable(torch.LongTensor(dec_partial_seq_mask))
+        dec_partial_seq = torch.LongTensor(dec_partial_seq)
+        dec_partial_seq_mask = torch.LongTensor(dec_partial_seq_mask)
 
         if opt.use_gpu:
             dec_partial_seq = dec_partial_seq.cuda()
@@ -160,7 +113,7 @@ def translate_batch(model, batch, opt, model_options):
             active_seq_data = original_seq_data.index_select(0, active_inst_idxs)
             active_seq_data = active_seq_data.view(*new_size)
 
-            return Variable(active_seq_data, volatile=True)
+            return active_seq_data
 
         def update_active_enc_info(enc_info_var, active_inst_idxs):
             ''' Remove the encoder outputs of finished instances in one batch. '''
@@ -175,7 +128,7 @@ def translate_batch(model, batch, opt, model_options):
             active_enc_info_data = original_enc_info_data.index_select(0, active_inst_idxs)
             active_enc_info_data = active_enc_info_data.view(*new_size)
 
-            return Variable(active_enc_info_data, volatile=True)
+            return active_enc_info_data
 
         src_pad_mask = update_active_seq(src_pad_mask, active_inst_idxs)
         enc_output = update_active_enc_info(enc_output, active_inst_idxs)
@@ -188,9 +141,10 @@ def translate_batch(model, batch, opt, model_options):
 
     for beam_idx in range(batch_size):
         scores, tail_idxs = beams[beam_idx].sort_scores()
-        all_scores += [scores[:opt.n_best]]
+        all_scores += [scores[0].item()]
 
-        hyps = [beams[beam_idx].get_hypothesis(i) for i in tail_idxs[:opt.n_best]]
+        hyps = beams[beam_idx].get_hypothesis(tail_idxs[0])
+        hyps = [k.item() for k in hyps]
         all_hyp += [hyps]
 
     return all_hyp, all_scores
@@ -204,44 +158,52 @@ def main():
     parser.add_argument('-save_result_file', required=True)
 
     parser.add_argument('-max_token_seq_len', type=int, required=True)
-    parser.add_argument('-n_best', type=int, default=1)
     parser.add_argument('-batch_size', type=int, default=64)
     parser.add_argument('-beam_size', type=int, default=20)
     parser.add_argument('-use_gpu', action='store_true')
     opt = parser.parse_args()
 
 
+    checkpoint = torch.load(opt.load_model_file, map_location=lambda storage, loc: storage)
+    model = checkpoint['model']
+    model_options = checkpoint['model_options']
+    if opt.use_gpu:
+        model = model.cuda()
+    model.prob_projection = nn.LogSoftmax(dim=1)
+    print('[INFO] loading model with parameter: {}'.format(model_options))
+
+
     decode_data = initialize_batch_loader(opt.read_decode_dir + '/feats.scp', opt.read_decode_dir + '/text', opt.read_vocab_file, opt.batch_size)
     print('[INFO] batch loader is initialized')
 
-    checkpoint = torch.load(opt.load_model_file)
-    model = checkpoint['model']
-    model_options = checkpoint['model_options']
-    print('[INFO] loading model with parameter: {}'.format(model_options))
 
-    model.prob_projection = nn.LogSoftmax()
-
-    if opt.use_gpu:
-        model = model.cuda()
-
-
-    word2idx = torch.load(opt.read_vocab_file)
+    word2idx = instances_handler.read_vocab(opt.read_vocab_file)
     idx2word = {index:word for word, index in word2idx.items()}
     with open(opt.save_result_file, 'wb') as f:
         for batch in tqdm(decode_data, mininterval=2, desc='(decode)'):
-            tgt = [triples[2] for triples in batch]
             all_hyp, all_scores = translate_batch(model, batch, opt, model_options)
-            for (a, b) in zip(tgt, all_hyp):
+            tgt_seq = batch[3]
+            for (a, b) in zip(tgt_seq, all_hyp):
                 print('next')
-                print(a[1:-1].tolist())
-                print(b[0][:-1])
+                a = a[np.where(a>3)] #remove function label 0,1,2,3 (currently, 0 as blank, 2 as start, 3 as end)
+                a = list(a)
+                b = b[:-1]
+                print(a)
+                print(b)
+
+                #a = [idx2word[index] if index in idx2word else constants.UNK_WORD for index in a]
+                #b = [idx2word[index] if index in idx2word else constants.UNK_WORD for index in b]
+                #print(' '.join(a))
+                #print(' '.join(b))
             exit(0)
+            '''
             for idx_seqs in all_hyp:
                 for idx_seq in idx_seqs:
                     word_seq = [idx2word[index] if index in idx2word else constants.UNK_WORD for index in idx_seq[:-1]]
                     pred_line = ' '.join(word_seq) + '\n'
                     f.write(pred_line.encode('utf-8'))
             exit(0)
+            '''
 
 
 if __name__ == '__main__':
