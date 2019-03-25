253424c18f74ae004a7a1bdfcd91b38cb8ff20eb
boji <boji@aliyun.com>
2019-03-25 13:53:15 +0800

    梯度正则化相关实验，效果不明显

--- project/attention-transformer-timit/local/pytorch/transformer/Modules.py
+++ project/attention-transformer-timit/local/pytorch/transformer/Modules.py
@@ -88,6 +88,28 @@ class ScaledDotProductAttention(nn.Module):
         attn = nn.functional.softmax(attn, dim=2)
         #incase condition of all -inf, in this condition, softmax will return nan and cause error
         attn.data.masked_fill_(attn_mask, 0)
+
+        #self.attn_norm_grad = self.get_norm_grad(attn)
+        #attn.register_hook(self.grad_hook)
+
         attn = self.dropout(attn)
         output = torch.bmm(attn, v)
         return output, attn
+
+    def get_norm_grad(self, attn):
+        size = attn.size()
+        T = torch.FloatTensor(range(size[2])).repeat(size[0], size[1], 1)
+        if attn.is_cuda:
+            T = T.cuda()
+        average = (attn * T).sum(2)
+        average = average.view(size[0], size[1], 1).repeat(1, 1, size[2])
+
+        scale = 0.03
+        norm_grad = ((T - average)/size[1]) ** 2 * scale
+        return norm_grad
+
+    def grad_hook(self, grad):
+        #torch.set_printoptions(precision=3, threshold=None, edgeitems=20, linewidth=None, profile=None)
+        #print(self.attn_norm_grad)
+        #exit(0)
+        grad += self.attn_norm_grad
